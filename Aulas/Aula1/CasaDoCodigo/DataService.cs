4ad4acd0adedd51bcf180b3a9649d83a9d3b2018
unknown <joao.kennedy707@gmail.com>
2023-01-24 03:36:34 -0300

    first commit

---
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,57 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto,
+            IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDb()
+        {
+            contexto.Database.Migrate();
+
+            //Lendo um arquivo json sobre livros
+            List<Livro> livros = GetLivros();
+
+            SaveProdutos(livros);
+        }
+
+        private void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros) // Inserindo os dados através de uma tabela produtos
+            {                         //Adicionar uma nova instancia de produto dentro da nossa lista de produto
+                contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+            }
+            contexto.SaveChanges();
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+    class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Esse método adiciona serviços
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlServer(connectionString)
+            );
+
+            //Adicionar uma instancia temporaria
+            services.AddTransient<IDataService, DataService>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+            services.AddTransient<IItemPedidoRepository, ItemPedidoRepository>();
+        }
+
+        // Esse método consome
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+
+            serviceProvider
+                .GetService<IDataService>().InicializaDb();
+        }
+    }
+}
